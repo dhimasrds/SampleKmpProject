b3753f55e9af0319c5d03abb92fe3241c4314671
dhimas.saputra <dhimas.saputra@finaccel.co>
2025-09-02 16:36:01 +0700

    Initial commit

---
+++ composeApp/src/iosMain/kotlin/com/example/samplekmpproject/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.example.samplekmpproject
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
---
+++ composeApp/src/jvmMain/kotlin/com/example/samplekmpproject/main.kt
@@ -0,0 +1,13 @@
+package com.example.samplekmpproject
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "SampleKmpProject",
+    ) {
+        App()
+    }
+}
